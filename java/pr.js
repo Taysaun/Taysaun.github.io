659cb8b278760d235ba382ac41a026f4b562bc6a
Taysaun <taysa25495@live.ytech.edu>
2023-09-29 11:17:46 -0400

    update

--- java/pr.js
+++ java/pr.js
@@ -1,25 +1,39 @@
-const money = () => {
-    var stuff = {
-        amount: 10,
-        paycheck: function (pay) {
-            this.amount = pay + this.amount
-        },
-    }
-    return stuff
+//Sets object that has an amount and methods to change the amount
+
+var bank = {
+    amount: 0,
+    //Adds money to the amount property
+    deposit(pay){
+        this.amount = pay + this.amount
+    },
+    //List that logs each deposit
+    payLog: []
+
 }
-var bank = money()
+//Adds money to the amount in the bank object
+//This code is ran when the submit button is clicked
 const addMoney = () => {
     var payAmount = Number(document.getElementById('payAmount').value)
-    payLog.push(payAmount)
-    console.log(payLog)
+    if (payAmount != NaN){
+        bank.payLog.push(payAmount)
+        for (i = bank.payLog.length - 1; i < bank.payLog.length; i++) {
+            bank.deposit(bank.payLog[i])
+        }
+        document.getElementById("amountBox").innerHTML = bank.amount
+        document.getElementById('payAmount').value = ''
+    };
 };
 
-var payLog = [134, 543, 345]
-for (i = 0; i < payLog.length; i++) {
-    bank.paycheck(payLog[i])
+const lastDeposit = () => {
+    for (i = 0; i < bank.payLog.length; i++){
+        document.getElementById('lastDeposit').innerHTML = bank.payLog[i]
+        console.log(bank.payLog[i])
+    }
 }
+const depositLog = () => {
+    document.getElementById('depositLog').innerHTML = bank.payLog.join('  ')
+};
 
 
-document.getElementById("amountBox").innerHTML = bank.amount
 
 
