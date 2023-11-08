0fd454e64f3803eb78f083261caef6da1b16794b
Taysaun <taysa25495@live.ytech.edu>
2023-11-08 10:38:49 -0500

    Reset

--- neeTemplate/fileRead.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-var data = fs.readFileSync("data.json");
-
-data = JSON.parse(data)
-
-data.taysaun += 1
-fs.writeFileSync("data.json", JSON.stringify(data));
-console.log(data)
