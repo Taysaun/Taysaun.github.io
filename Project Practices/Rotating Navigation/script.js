641b927b9e0863c3e56fa24beadacfc0cd131503
Taysaun <taysa25495@live.ytech.edu>
2024-08-13 16:58:04 -0400

    Hidden widget

---
+++ Project Practices/Rotating Navigation/script.js
@@ -0,0 +1,13 @@
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+
+open.addEventListener('click', () => {
+    container.classList.add('show-nav')
+    document.body.style.overflowY = "hidden"
+})
+
+close.addEventListener('click', () => {
+    container.classList.remove('show-nav')
+    document.body.style.overflowY = "visible"
+})
