d4192e5a0072cf76b78e1c10fef46397cd61913c
Taysaun <taysa25495@live.ytech.edu>
2024-12-19 12:23:16 -0400

    update boiler

---
+++ updateBoilerplate/modules/socket.js
@@ -0,0 +1,10 @@
+function connection(socket) {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+}
+
+module.exports = {
+    connection
+};
