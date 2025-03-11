d0182432764f72cc23013f950f763a9507d07862
Taysaun Johnson <taysa25495@live.ytech.edu>
2025-03-11 11:22:39 -0400

    Update app.js

--- ConsoleDataEntry/app.js
+++ ConsoleDataEntry/app.js
@@ -5,6 +5,33 @@ var interface = readline.createInterface({
     input, output
 })
 
-interface.question('What do you like to do?', function (line) {
-    console.log('Line: ' + line)
-})
+let orders = [];
+
+function run() {
+    interface.write(" 1. Start New Order \n 2. Edit Existing Order \n 3. Show All Orders \n")
+    
+    interface.question('What would you like to do? ', function (answer) {
+        console.log('You answered ' + answer)
+        switch (answer) {
+            case "1":
+                interface.question("Enter Name: ", (answer) => {
+                    username = answer
+                    interface.question("Enter Address: ", (answer) => {
+                        address = answer
+                        orders.push({
+                            name: username,
+                            address: address
+                        })
+                        run()
+                    })
+                })
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+        }
+    })
+}
+
+run()
